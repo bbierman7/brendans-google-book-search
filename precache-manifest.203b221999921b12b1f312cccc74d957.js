34adb23177381b291b791a375b88381dd2821f91
bbierman <bbierman7@gmail.com>
2019-05-01 01:08:42 -0500

    Updates

---
+++ precache-manifest.203b221999921b12b1f312cccc74d957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36242ea929d1861e0180d74229932faa",
+    "url": "/google-books-search/index.html"
+  },
+  {
+    "revision": "774b36d6f5b8c2e8d703",
+    "url": "/google-books-search/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/google-books-search/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "774b36d6f5b8c2e8d703",
+    "url": "/google-books-search/static/js/main.eecd6ccb.chunk.js"
+  },
+  {
+    "revision": "d672f040abfa3c0ea28a",
+    "url": "/google-books-search/static/js/runtime~main.9ad78463.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-books-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
